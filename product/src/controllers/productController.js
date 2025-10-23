3935533f25073bab497137c91058f0ab9c39e804
vanTruong179 <vantruong1305.vn@gmail.com>
2025-10-23 23:50:09 +0700

    done project

--- product/src/controllers/productController.js
+++ product/src/controllers/productController.js
@@ -109,17 +109,16 @@ class ProductController {
       res.status(500).json({ message: "Server error" });
     }
   }
-  //học để kiểm tra
+  //
   async getProductById(req, res) {
     try {
       const product = await Product.findById(req.params.id);
-      if (!product) return res.status(404).json({ message: "Không tìm thấy sản phẩm" });
+      if (!product) return res.status(404).json({ message: "Product not found" });
       res.status(200).json(product);
-    } catch (error) {
+    } catch (err) {
       res.status(500).json({ message: "Server error" });
     }
   }
-  //
 
 }
 
